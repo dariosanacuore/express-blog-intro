5203b96954762612a4b787f0571a829928a49ef5
dariosanacuore <dariosanacuore2@gmail.com>
2025-12-16 16:02:50 +0100

    feat:Creato array

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import express from "express";
+import express from "express"
+import posts from "./array/posts.js"
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,10 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
     res.send("Server del mio blog");
+})
+
+app.get('/posts', (req, res) => {
+    res.json(posts);
 });
 
 
