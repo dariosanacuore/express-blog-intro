6e395ed1aa7e2ac321aa44bc9a6e4258cb66e7ee
dariosanacuore <dariosanacuore2@gmail.com>
2025-12-16 16:30:04 +0100

    feat:Completato l'aggiunta delle immagini

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express"
 import posts from "./array/posts.js"
+import fs from 'fs';
+
 
 const app = express();
 const port = 3000;
@@ -12,8 +14,28 @@ app.get("/", (req, res) => {
 
 app.get('/posts', (req, res) => {
     res.json(posts);
-});
+})
 
+
+app.get('/bacheca', (req, res) => {
+    const immagini = fs.readdirSync('public/img');
+
+    const newArray = [];
+
+    for (let i = 0; i < posts.length; i++) {
+        const post = {
+            ...posts[i],
+            img: `/img/${immagini[i]}`
+        };
+
+        newArray.push(post);
+    }
+
+    res.json({
+        conteggio: newArray.length,
+        lista: newArray
+    });
+});
 
 app.listen(port, () => {
     console.log("In ascolto dalla porta ", port);
--- array/posts.js
+++ array/posts.js
@@ -2,31 +2,26 @@ const posts = [
     {
         titolo: "Viaggio in montagna",
         contenuto: "La montagna è il luogo ideale per rilassarsi e fare trekking immersi nella natura.",
-        immagine: "images/montagna.jpg",
         tags: ["viaggio", "natura", "montagna", "trekking"]
     },
     {
         titolo: "Ricetta della pizza fatta in casa",
         contenuto: "Scopri come preparare una pizza deliziosa con ingredienti freschi e semplici.",
-        immagine: "images/pizza.jpg",
         tags: ["cucina", "ricetta", "pizza", "cibo"]
     },
     {
         titolo: "Consigli per la fotografia notturna",
         contenuto: "Impara le tecniche base per catturare splendide foto durante la notte.",
-        immagine: "images/fotografia.jpg",
         tags: ["fotografia", "notte", "tecniche", "consigli"]
     },
     {
         titolo: "Allenamento a casa senza attrezzi",
         contenuto: "Ecco una routine completa per restare in forma anche senza andare in palestra.",
-        immagine: "images/allenamento.jpg",
         tags: ["fitness", "casa", "allenamento", "salute"]
     },
     {
         titolo: "Guida ai film di fantascienza",
         contenuto: "Una lista dei migliori film di fantascienza da vedere almeno una volta nella vita.",
-        immagine: "images/film.jpg",
         tags: ["cinema", "fantascienza", "film", "guida"]
     }
 ];
